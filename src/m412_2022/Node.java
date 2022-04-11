04498ed5839c0a768729f5aed640f0ea09bd8c84
HOGIE Luc <luc.hogie@inria.fr>
2022-04-11 11:14:49 +0200

    file request

--- src/m412_2022/Node.java
+++ src/m412_2022/Node.java
@@ -1,28 +1,17 @@
 package m412_2022;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
-
-import org.nustaq.serialization.FSTConfiguration;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class Node {
 	private final DatagramSocket socket;
@@ -31,53 +20,23 @@ public class Node {
 	private final int port;
 	private final Set<Long> alreadyReceivedMessages = new HashSet<>();
 	private final String nickname;
-	private static final Random random = new Random();
+	static final Random random = new Random();
 	Serializer serializer = new FSTSerializer();
-
-	// the base abstraction of a message does not define any content or role
-	public static class Message implements Serializable {
-		// just an randomly chosen ID
-		public long id = random.nextLong();
-
-		// and a list of node that relayed the message
-		public List<String> route = new ArrayList<>();
-	}
-
-	// a specific type of message that carries a string
-	public static class TextMessage extends Message {
-		public String text;
-
-		@Override
-		public String toString() {
-			return route + "> " + text;
-		}
-	}
-
-	// a specific type of message that is an ACK of an other message
-	// whose it carries the ID
-	public static class ACKMessage extends Message {
-		// the ID of the message we are interested in
-		public long originalMessageID;
-
-		// the list of nodes that had received that message
-		public List<String> actualRecipients = new ArrayList<>();
-
-		@Override
-		public String toString() {
-			return "ACK received from  " + route.get(0) + " who informs us the message reached " + actualRecipients;
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		System.out.println("Java version: " + System.getProperty("java.version"));
-		new Node(6665, System.getProperty("user.name"));
-	}
+	File directory = new File(System.getProperty("user.home") + "/" + "m412");
+	
+	
 
 	public Node(int port, String nickname) throws SocketException, UnknownHostException {
 		this.socket = new DatagramSocket(port);
 		this.port = port;
 		this.nickname = nickname;
 
+		if (!directory.exists()) {
+			System.out.println("creating directory " + directory.getAbsolutePath());
+			directory.mkdirs();
+		}
+		
+		
 		final String prefix = "192.168.225.";
 
 		for (var suffix : new int[] { 56, 137, 36, 28, 54, 129, 167, 34, 79, 164, 75, 4, 230, 112 }) {
@@ -98,9 +57,28 @@ public class Node {
 			try {
 				while (true) {
 					System.out.println("Please type a message: ");
-					var msg = new TextMessage();
-					msg.text = userInput.nextLine();
-					sendToAllPeers(msg);
+					var line = userInput.nextLine();
+
+					if (line.startsWith("/")) {
+						line = line.substring(1);
+						var tokens = line.split(" +");
+
+						if (tokens.length == 0) {
+							System.err.println("command not specified");
+						} else {
+							var cmd = tokens[0];
+
+							if (cmd.equals("list")) {
+								sendToAllPeers(new FileListRequest());
+							} else {
+								System.err.println("Unknown command " + cmd);
+							}
+						}
+					} else {
+						var msg = new TextMessage();
+						msg.text = line;
+						sendToAllPeers(msg);
+					}
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -131,6 +109,10 @@ public class Node {
 							System.out.println(msg);
 						} else if (msg instanceof ACKMessage) {
 							System.out.println(msg);
+						} else if (msg instanceof FileListRequest) {
+							var r = new FileListResponse();
+							r.filenames = Arrays.asList(directory.list());
+							System.out.println(msg);
 						} else {
 							throw new IllegalStateException("unknown message type:  " + msg.getClass().getName());
 						}
@@ -156,74 +138,6 @@ public class Node {
 			DatagramPacket packet = new DatagramPacket(buf, buf.length, peer, port);
 			socket.send(packet);
 			packet = new DatagramPacket(buf, buf.length);
-		}
-	}
-
-	public static interface Serializer {
-		Message deserializeMessage(byte[] bytes) throws IOException;
-
-		byte[] serialize(Message msg) throws IOException;
-	}
-
-	public static class JavaSerializer implements Serializer {
-
-		@Override
-		public Message deserializeMessage(byte[] bytes) throws IOException {
-			var bis = new ByteArrayInputStream(bytes);
-			var ois = new ObjectInputStream(bis);
-
-			try {
-				var m = (Message) ois.readObject();
-				ois.close();
-				return m;
-			} catch (ClassNotFoundException e) {
-				throw new IOException(e);
-			}
-		}
-
-		@Override
-		public byte[] serialize(Message msg) throws IOException {
-			var bos = new ByteArrayOutputStream();
-			var oos = new ObjectOutputStream(bos);
-			oos.writeObject(msg);
-			return bos.toByteArray();
-		}
-	}
-
-	public static class JSONSerializer implements Serializer {
-		static final Gson gson;
-
-		static {
-			GsonBuilder builder = new GsonBuilder();
-			builder.setPrettyPrinting();
-			gson = builder.create();
-		}
-
-		@Override
-		public Message deserializeMessage(byte[] bytes) throws IOException {
-			String received = new String(bytes);
-
-			// deserialize the UDP packet to a Message object
-			return gson.fromJson(received, Message.class);
-		}
-
-		@Override
-		public byte[] serialize(Message msg) throws IOException {
-			return gson.toJson(msg).getBytes();
-		}
-	}
-
-	public static class FSTSerializer implements Serializer {
-		static FSTConfiguration conf = FSTConfiguration.createDefaultConfiguration();
-
-		@Override
-		public Message deserializeMessage(byte[] bytes) throws IOException {
-			return (Message) conf.asObject(bytes);
-		}
-
-		@Override
-		public byte[] serialize(Message msg) throws IOException {
-			return conf.asByteArray(msg);
 		}
 	}
 }
