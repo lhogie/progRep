8e186c411fe290d3b3c7f99d409996f95642da9b
HOGIE Luc <luc.hogie@inria.fr>
2022-05-09 12:18:34 +0200

    with some comments

--- src/m412_2022/Batch.java
+++ src/m412_2022/Batch.java
@@ -8,13 +8,15 @@ import java.util.concurrent.Callable;
 
 public class Batch {
 	public static void main(String[] args) throws InterruptedException {
-		var batch = new BatchSystem<String>();
+		// create a new batch system of 10 threads that will generate string results
+		var batch = new BatchSystem<String>(10);
 
+		// send 100 jobs into the system
 		for (int i = 0; i < 100; ++i) {
 			System.out.println("injecting jobs");
 			batch.in.put(() -> {
-				// some long computation that takes 1s
 				try {
+					// some long computation that takes between 0 and 10s
 					long duration = (long) (Math.random() * 10000);
 					Thread.sleep(duration);
 				} catch (InterruptedException e) {
@@ -25,6 +27,7 @@ public class Batch {
 			});
 		}
 
+		// prints results as soon as they are available
 		while (true) {
 			var j = batch.results.take();
 			System.out.println("new result: " + j);
@@ -32,14 +35,17 @@ public class Batch {
 	}
 
 	static class BatchSystem<R> {
+		// where submitted jobs will wait
 		BlockingQueue<Callable<R>> in = new ArrayBlockingQueue(1000);
+		
+		// where results will be put after processing
 		BlockingQueue<String> results = new ArrayBlockingQueue(1000);
 
-		public BatchSystem() {
+		public BatchSystem(int nbThreads) {
 			List<Cell> cells = new ArrayList<>();
 
 			System.out.println("creating cells");
-			while (cells.size() < 10) {
+			while (cells.size() < nbThreads) {
 				cells.add(new Cell(in, results));
 			}
 
@@ -49,21 +55,26 @@ public class Batch {
 	}
 
 	static class Cell<R> extends Thread {
-		final BlockingQueue<Callable<R>> in;
-		final BlockingQueue<R> out;
+		final BlockingQueue<Callable<R>> incomingJobs;
+		final BlockingQueue<R> results;
 
 		Cell(BlockingQueue<Callable<R>> in, BlockingQueue<R> out) {
-			this.in = in;
-			this.out = out;
+			this.incomingJobs = in;
+			this.results = out;
 		}
 
 		@Override
 		public void run() {
 			while (true) {
 				try {
-					var job = in.take();
+					// takes a jobs, waits if necessary
+					var job = incomingJobs.take();
+					
+					// process it
 					R r = job.call();
-					out.put(r);
+					
+					// put the result in the result queue
+					results.put(r);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				} catch (Exception e) {
